ce23286b5cb94a7642677716d301e470820c8a4a
Ivzond <141146338+Ivzond@users.noreply.github.com>
2024-03-28 21:50:15 +0300

    Add files via upload

---
+++ qt_client/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "QtNetwork"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void createStudent();
+    void uploadPhoto();
+    void readStudents();
+    void deleteStudent();
+    void openStudentCreationDialog();
+    void handleStudentCreation();
+    void replyFinished(QNetworkReply *reply);
+
+//    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QNetworkAccessManager *manager;
+};
+#endif // MAINWINDOW_H
