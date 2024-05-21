444393a2aa9c272cbfd551ea790e04064ba48ccc
Ivzond <141146338+Ivzond@users.noreply.github.com>
2024-05-21 17:23:37 +0300

    Update test_main.py

--- server/test/test_main.py
+++ server/test/test_main.py
@@ -1,23 +1,23 @@
 import unittest
-import httpx
 from fastapi.testclient import TestClient
-from server.src.http_server.main import app
+from ..src.http_server.main import app
 
 
 class TestMain(unittest.TestCase):
 
     def setUp(self):
         self.client = TestClient(app)
-        self.auth = ('admin', 'password_hash')  # Use plain password for the test
+        self.auth = ('admin', 'password_hash')
 
-    def test_create_student(self):
-        response = self.client.post("/students/", json={"name": "John Doe"}, auth=self.auth)
+    def test_create_student(self):        # Include required fields in the request data
+        data = {"name": "Ivan", "date_of_birth": "2000-01-01", "grade": 3, "student_group": "1408"}
+        response = self.client.post("/students/", json=data, auth=self.auth)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), "Student created successfully")
 
     def test_upload_photo(self):
-        with open("test_photo.jpg", "rb") as photo:
-            response = self.client.post("/students/1/photo", files={"photo": photo}, auth=self.auth)
+        photo_content = b"some_test_photo_data"
+        response = self.client.post("/students/1/photo", files={"photo": (photo_content, "test.jpg")}, auth=self.auth)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), "Photo uploaded successfully")
 
@@ -35,6 +35,7 @@ class TestMain(unittest.TestCase):
         response = self.client.delete("/students/1", auth=self.auth)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), "Deleted successfully")
+
 
 if __name__ == "__main__":
     unittest.main()
